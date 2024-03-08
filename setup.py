e9fb42b398bc5c9ee55543cdb49c67772bbadd33
C0MaE <89319337+C0MaE@users.noreply.github.com>
2024-03-08 12:04:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IsarnConnect',
-    version='0.5',
+    version='0.7',
     author='C0MaE',
     author_email='jannis.koberg@icloud.com',
     packages=find_packages(),
